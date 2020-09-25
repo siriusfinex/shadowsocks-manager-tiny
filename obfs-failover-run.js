f59e09816a17b39bbef3c1186adfa2c647bf0618
sean <31554433+siriusfinex@users.noreply.github.com>
2020-09-25 17:53:39 +0800

    Add files via upload

---
+++ obfs-failover-run.js
@@ -0,0 +1,42 @@
+const spawn = require('child_process').spawn;
+
+let run = false;
+let runParams = 'libev:aes-256-cfb';
+let ssConfig = '127.0.0.1:6001';
+const argv = process.argv.filter((ele, index) => index > 1);
+argv.forEach((f, index) => {
+  if(f === '--run' || f === '-r') {
+    run = true;
+    if(argv[index + 1] && !argv[index + 1].startsWith('-')) { runParams = argv[index + 1]; }
+  }
+  if(f === '--shadowsocks' || f === '-s') {
+    ssConfig = argv[index + 1];
+  }
+});
+if(!run) { return; }
+let type = 'libev';
+let method = 'aes-256-cfb';
+if(runParams.indexOf(':') >= 0) {
+  method = runParams.split(':')[1];
+}
+let shadowsocks;
+if(runParams.indexOf('python') >= 0) {
+  type = 'python';
+  const tempPassword = 'qwerASDF' + Math.random().toString().substr(2, 8);
+  shadowsocks = spawn('ssserver', ['-m', method, '-p', '65535', '-k', tempPassword, '--manager-address', ssConfig, '-t', '600', '--fast-open' ]);
+} else {
+  shadowsocks = spawn('ss-manager', ['-m', method, '-u', '--manager-address', ssConfig, '-t', '600', '--plugin', 'obfs-server', '--plugin-opts', 'obfs=tls;failover=www.bing.com:443' ]);
+}
+
+shadowsocks.stdout.on('data', (data) => {
+  // console.log(`stdout: ${data}`);
+});
+
+shadowsocks.stderr.on('data', (data) => {
+  // console.error(`stderr: ${data}`);
+});
+
+shadowsocks.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
+console.log(`Run shadowsocks (${ type === 'python' ? 'python' : 'libev'})`);
